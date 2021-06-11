9821d7bf823ed3dc27640f85f790561b220c7f70
Tiago Rodrigues <mail@tig.pt>
2021-06-11 13:27:19 +0100

    APM configs place

--- app/app.py
+++ app/app.py
@@ -6,6 +6,8 @@ from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
 
 APP = Flask(__name__)
+APP.config['ELASTIC_APM'] = {
+}
 apm = ElasticAPM(APP)
 APP.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
